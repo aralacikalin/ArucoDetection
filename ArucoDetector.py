1cd5b1c7fc64e9e38a87837e576e716e58e74799
Aral Acikalin <aralacikalin@hotmail.com>
2024-01-26 18:58:34 +0200

    initial commit

---
+++ ArucoDetector.py
@@ -0,0 +1,95 @@
+import argparse
+import imutils
+import cv2
+import sys
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+print("[INFO] loading image...")
+image = cv2.imread("aruco\TEST9.jpg")
+# image = cv2.imread("aruco\TestImageDifferentColors.png")
+# image = imutils.resize(image, width=600)
+# verify that the supplied ArUCo tag exists and is supported by
+# OpenCV
+
+arucoDict="DICT_4X4_1000"
+if ARUCO_DICT.get(arucoDict, None) is None:
+	print("[INFO] ArUCo tag of '{}' is not supported".format(
+		arucoDict))
+	sys.exit(0)
+# load the ArUCo dictionary, grab the ArUCo parameters, and detect
+# the markers
+print("[INFO] detecting '{}' tags...".format(arucoDict))
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[arucoDict])
+arucoParams = cv2.aruco.DetectorParameters_create()
+arucoParams.minMarkerPerimeterRate=0.01
+(corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
+	parameters=arucoParams)
+
+rejectedImage=image.copy()
+cv2.aruco.drawDetectedMarkers(rejectedImage, rejected, borderColor=(100, 0, 240))
+
+    # verify *at least* one ArUco marker was detected
+if len(corners) > 0:
+	# flatten the ArUco IDs list
+	ids = ids.flatten()
+	# loop over the detected ArUCo corners
+	for (markerCorner, markerID) in zip(corners, ids):
+		# extract the marker corners (which are always returned in
+		# top-left, top-right, bottom-right, and bottom-left order)
+		corners = markerCorner.reshape((4, 2))
+		(topLeft, topRight, bottomRight, bottomLeft) = corners
+		# convert each of the (x, y)-coordinate pairs to integers
+		topRight = (int(topRight[0]), int(topRight[1]))
+		bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+		bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+		topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+		# draw the bounding box of the ArUCo detection
+		cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+		cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+		cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+		cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+		# compute and draw the center (x, y)-coordinates of the ArUco
+		# marker
+		cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+		cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+		cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+		# draw the ArUco marker ID on the image
+		cv2.putText(image, str(markerID),
+			(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, (0, 255, 0), 2)
+		print("[INFO] ArUco marker ID: {}".format(markerID))
+		# show the output image
+
+# cv2.namedWindow("Rejected Markers", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("Rejected Markers",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+# cv2.namedWindow("Detected Markers", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("Detected Markers",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+
+cv2.imshow("Rejected Markers", rejectedImage)
+cv2.imshow("Detected Markers", image)
+cv2.waitKey(0)
+cv2.imwrite("./DifferentColorMarkers-Smaller.jpg",image)
